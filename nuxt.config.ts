0da714ce3da4a0a0654265ebc7fb08a1ffc3969a
peter910820 <peter910820@gmail.com>
2025-06-12 23:29:56 +0800

    chore: remove redundant js

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,6 @@ export default defineNuxtConfig({
         { src: 'https://cdn.jsdelivr.net/npm/wowjs@1.1.3/dist/wow.min.js', defer: true },
         { src: 'https://kit.fontawesome.com/8f1caa7fe1.js', crossorigin: "anonymous", defer: true },
         { src: 'https://code.jquery.com/jquery-3.6.0.min.js', defer: true },
-        { src: 'https://kit.fontawesome.com/8f1caa7fe1.js', defer: true },
         { src: 'https://unpkg.com/showdown@2.1.0/dist/showdown.min.js', defer: true },
         { src: 'https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js', defer: true },
         { src: 'https://unpkg.com/@highlightjs/cdn-assets@11.7.0/highlight.min.js', defer: true },
