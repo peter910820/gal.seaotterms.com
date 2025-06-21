c35e4d41102de51390e8364aab27cfa1a21f299d
peter910820 <peter910820@gmail.com>
2025-06-21 11:51:18 +0800

    chore: set eslint, prettier and batch

--- app.vue
+++ app.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import NavBar from './components/NavBar.vue';
-
+import NavBar from "./components/NavBar.vue";
 </script>
 
 <template>
+  <NavBar />
   <div>
-    <NavBar />
     <NuxtPage />
   </div>
 </template>
